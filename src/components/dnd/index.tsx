ab9ec5b45ddb51af2a2ef3ade6a42b25dafe298d
r-khosravi <r.khosravi@amnpardaz.com>
2023-05-24 20:46:15 +0430

    add grid layout

--- src/components/dnd/index.tsx
+++ src/components/dnd/index.tsx
@@ -8,6 +8,25 @@ import EditIcon from "@mui/icons-material/Edit";
 import { deleteChartHandler } from "../../services/redux/features/charts";
 import { Link } from "react-router-dom";
 import { MoveElement } from "../moveElement/MoveElement";
+import { Responsive, WidthProvider } from "react-grid-layout";
+const ResponsiveGridLayout = WidthProvider(Responsive);
+
+var layout = [
+  { i: "a", x: 0, y: 0, w: 4, h: 1 },
+  { i: "b", x: 4, y: 0, w: 4, h: 1 },
+  { i: "c", x: 8, y: 0, w: 4, h: 1 },
+  { i: "d", x: 0, y: 1, w: 4, h: 1 },
+  { i: "e", x: 4, y: 1, w: 4, h: 1 },
+  { i: "f", x: 8, y: 1, w: 4, h: 1 },
+];
+var layout1 = [
+  { i: "a", x: 0, y: 0, w: 6, h: 1 },
+  { i: "b", x: 6, y: 0, w: 6, h: 1 },
+  { i: "c", x: 0, y: 1, w: 6, h: 1 },
+  { i: "d", x: 6, y: 1, w: 6, h: 1 },
+  { i: "e", x: 0, y: 2, w: 6, h: 1 },
+  { i: "f", x: 6, y: 2, w: 6, h: 1 },
+];
 
 export const DND = () => {
   const dispatch = useAppDispatch();
@@ -46,15 +65,38 @@ export const DND = () => {
 
   return (
     <Grid spacing={3} container sx={{ position: "relative" }}>
+      <ResponsiveGridLayout
+        className="layout"
+        layouts={{ lg: layout }}
+        breakpoints={{ lg: 1200 }}
+        cols={{ lg: 12 }}
+        rowHeight={281}
+        width={1200}
+      >
+        <div key="a" style={{ backgroundColor: "yellow" }}>
+          <div>card</div>
+        </div>
+        <div key="b" style={{ backgroundColor: "green" }}>
+          <div>card</div>
+        </div>
+        <div key="c" style={{ backgroundColor: "red" }}>
+          <div>card</div>
+        </div>
+        <div key="d" style={{ backgroundColor: "blue" }}>
+          <div>card</div>
+        </div>
+        <div key="e" style={{ backgroundColor: "violet" }}>
+          <div>card</div>
+        </div>
+        <div key="f" style={{ backgroundColor: "lemonchiffon" }}>
+          <div>card</div>
+        </div>
+      </ResponsiveGridLayout>
       {grid.map((item: any) => (
         <Grid
           item
           xs={6}
           key={item.id}
-          draggable
-          onDragStart={(event) => handleDragStart(event, item.id)}
-          onDragOver={handleDragOver}
-          onDrop={(event) => handleDrop(event, item.id)}
           sx={{ minHeight: "200px", cursor: "pointer" }}
         >
           <MoveElement>
--- src/components/moveElement/MoveElement.tsx
+++ src/components/moveElement/MoveElement.tsx
@@ -5,40 +5,39 @@ type MoveElementProps = {
 };
 
 export const MoveElement = ({ children }: MoveElementProps) => {
+
   const [position, setPosition] = useState({ x: 0, y: 0 });
-  const elementRef = useRef<HTMLInputElement>(null);
 
-  const handleMouseDown = (event: any) => {
-    event.preventDefault();
-    document.addEventListener("mousemove", handleMouseMove);
-    document.addEventListener("mouseup", handleMouseUp);
-  };
-
-  const handleMouseMove = (event: any) => {
-    const element = elementRef.current;
-    if (element) {
-      const rect = element.getBoundingClientRect();
-      const x = event.clientX - rect.width / 2;
-      const y = event.clientY - rect.height / 2;
-      setPosition({ x, y });
-    }
-  };
-
-  const handleMouseUp = () => {
-    document.removeEventListener("mousemove", handleMouseMove);
-    document.removeEventListener("mouseup", handleMouseUp);
-  };
 
   return (
     <div
-      ref={elementRef}
-      onMouseDown={handleMouseDown}
-      style={{
-        position: "relative",
-        top: position.y,
-        left: position.x,
-        height: "auto",
-        backgroundColor: "red",
+      style={{ position: "relative", left: position.x, top: position.y }}
+      onMouseDown={(e) => {
+        // Get the initial mouse position
+        const startX = e.pageX - position.x;
+        const startY = e.pageY - position.y;
+
+        const handleMouseMove = (e: any) => {
+          setPosition({
+            x: e.pageX - startX,
+            y: e.pageY - startY,
+          });
+        };
+
+        const handleMouseUp = (e) => {
+          document.removeEventListener("mousemove", handleMouseMove);
+          document.removeEventListener("mouseup", handleMouseUp);
+          e.target.style.zIndex = 0;
+        };
+
+        // Add an event listener to move the element on mousemove
+        document.addEventListener("mousemove", handleMouseMove);
+
+        // Remove the event listener on mouseup
+        document.addEventListener("mouseup", handleMouseUp);
+
+        // Set the zIndex of the element to make it appear above other elements
+        e.target.style.zIndex = 1;
       }}
     >
       {children}
