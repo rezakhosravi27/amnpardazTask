c6e3e28a9a3b840bc6be16c3e2dbb36f3803bc4f
r-khosravi <r.khosravi@amnpardaz.com>
2023-05-30 16:04:35 +0430

    debug legend

--- src/components/nivoCharts/pieChart/PieChart.tsx
+++ src/components/nivoCharts/pieChart/PieChart.tsx
@@ -98,31 +98,35 @@ export const PieChart = ({ chartData }: LineChartProps) => {
           id: "lines",
         },
       ]}
-      legends={[
-        {
-          anchor: "bottom",
-          direction: "row",
-          justify: false,
-          translateX: 0,
-          translateY: 56,
-          itemsSpacing: 0,
-          itemWidth: 100,
-          itemHeight: 18,
-          itemTextColor: "#999",
-          itemDirection: "left-to-right",
-          itemOpacity: 1,
-          symbolSize: 18,
-          symbolShape: "circle",
-          effects: [
-            {
-              on: "hover",
-              style: {
-                itemTextColor: "#fff",
+      legends={
+        chartData.legend == "show"
+          ? [
+              {
+                anchor: "bottom",
+                direction: "row",
+                justify: false,
+                translateX: 0,
+                translateY: 56,
+                itemsSpacing: 0,
+                itemWidth: 100,
+                itemHeight: 18,
+                itemTextColor: "#999",
+                itemDirection: "left-to-right",
+                itemOpacity: 1,
+                symbolSize: 18,
+                symbolShape: "circle",
+                effects: [
+                  {
+                    on: "hover",
+                    style: {
+                      itemTextColor: "#fff",
+                    },
+                  },
+                ],
               },
-            },
-          ],
-        },
-      ]}
+            ]
+          : []
+      }
     />
   );
 };
